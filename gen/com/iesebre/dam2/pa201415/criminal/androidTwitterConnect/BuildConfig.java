2e693e57149d3aef67a85a8c207fce375e5c5806
kriminal666 <piltrafiya_666@hotmail.com>
2014-11-30 12:58:28 +0100

    AndroidTwitterConnect

---
+++ gen/com/iesebre/dam2/pa201415/criminal/androidTwitterConnect/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.iesebre.dam2.pa201415.criminal.androidTwitterConnect;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
